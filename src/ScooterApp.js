941ea68e2085e49fbdaad20d3cfca7a69162b247
Rtollinchi <rtollinchi23@gmail.com>
2024-09-13 13:11:45 -0400

    add registerUser method

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -6,11 +6,29 @@ const Scooter = require("./Scooter");
 class ScooterApp {
   constructor() {
     this.stations = {
-      Dumbo: [],
-      "Brooklyn Bridge": [],
-      Williamsburg: [],
+      'Dumbo': [],
+      'Brooklyn Bridge': [],
+      'Williamsburg': [],
     };
     this.registeredUsers = {};
   }
+
+  registerUser(username, password, age) {
+    if (this.registeredUsers[username]) {
+      throw new Error("User already registered");
+    }
+    if (age < 18) {
+      throw new Error("Must be over the age of 18 to ride");
+    }
+
+    const newUser = new User(username, password, age);
+    this.registeredUsers[username] = newUser;
+    return newUser;
+  }
 }
+
+// const scooterApp = new ScooterApp()
+// const newUser = scooterApp.registerUser("Crew", "Monsterjam", 4);
+
+// console.log(newUser);
 module.exports = ScooterApp;
