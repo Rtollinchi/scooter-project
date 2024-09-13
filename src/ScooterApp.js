416e6360e2520fd4664abb234a2b7f44e5f0e280
Rtollinchi <rtollinchi23@gmail.com>
2024-09-13 16:38:29 -0400

    Add loginUser method

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -11,7 +11,7 @@ class ScooterApp {
       'Williamsburg': [],
     };
     this.registeredUsers = {};
-  }
+  };
 
   registerUser(username, password, age) {
     if (this.registeredUsers[username]) {
@@ -20,15 +20,32 @@ class ScooterApp {
     if (age < 18) {
       throw new Error("Must be over the age of 18 to ride");
     }
-
     const newUser = new User(username, password, age);
+
     this.registeredUsers[username] = newUser;
     return newUser;
+  };
+
+  loginUser(username, password) {
+   const user = this.registeredUsers[username];
+
+   if (!user) {
+    throw new Error('Incorrect username or password');
+   } if (user.password !== password) {
+    throw new Error('Incorrect username or password');
+   }
+
+   user.login(password)
+    console.log('user has been logged in');
   }
 }
 
-// const scooterApp = new ScooterApp()
-// const newUser = scooterApp.registerUser("Crew", "Monsterjam", 4);
+const scooter = new ScooterApp()
 
-// console.log(newUser);
+scooter.registerUser('Crew', 'Monsterjam', 22);
+
+scooter.loginUser('Crew', 'Monsterjam');
+
+console.log(scooter.registeredUsers['Crew'])
+console.log(this.registeredUsers)
 module.exports = ScooterApp;
