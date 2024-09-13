48748f30d48dd150b5c37016f0e9805fb68aca2e
Rtollinchi <rtollinchi23@gmail.com>
2024-09-12 22:25:56 -0400

    add scooter.js class and rent() method.  Created first test in scooter.test if scooter is charged and not broken.

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,29 @@
-class Scooter {
-  // scooter code here
-}
+/* eslint-disable */
 
-module.exports = Scooter
+class Scooter {
+  static nextSerial = 1;
+
+  constructor(station) {
+    this.station = station;
+    this.user = null;
+    this.serial = Scooter.nextSerial++;
+    this.charge = 100;
+    this.isBroken = false;
+  }
+  rent(user) {
+    if (this.charge > 20 && !this.isBroken) {
+      if (this.station) {
+        this.station = null;
+        this.user = user;
+      } else {
+        throw new Error('Scooter is already rented')
+      }
+    } else if (this.charge <= 20) {
+        throw new Error("Scooter needs to charge");
+      } else {
+        throw new Error("Scooter needs repair");
+      }
+    }
+  };
+
+module.exports = Scooter;
